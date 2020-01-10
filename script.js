bf4dca70f1980fb0dcb0759e24a18d35b3d58b9d
nola360 <adaramolajr@gmail.com>
2020-01-09 21:22:59 -0500

    Creating Replaceing adding removing Elements

--- script.js
+++ script.js
@@ -1,36 +1,78 @@
-// Create an Element
+// Replace Element
 
-const li = document.createElement('li');
-
-// Add Class
-li.className = 'collection-item';
-console.log(li);
-
-// Add Id
-li.id = 'new-item';
-console.log(li);
-
-// Add Attribute
-li.setAttribute('title', 'New Item');
-console.log(li);
-
-// Create a text Node & append
-li.appendChild(document.createTextNode('Hello Wolrd!'));
-console.log(li);
-
-// Create New Link Element
-const link = document.createElement('a');
-
-// Add Classes
-link.className = 'delete-item secondary-content';
-
-// Add icon HTML
-link.innerHTML = '<i class="fa fa-remove"></i>'
-
-// Append link into li
-li.appendChild(link);
+// Create Element
+const newHeading = document.createElement('h2');
 
 
-// Append li as child to ul
-document.querySelector('ul.collection').appendChild(li);
-console.log(li);
+// Add id
+newHeading.id = 'task-title';
+console.log(newHeading);
+
+// New Text Node
+newHeading.appendChild(document.createTextNode('Task LLst'));
+
+// Get old heading
+const oldHeading = document.getElementById('task-title');
+console.log(oldHeading);
+
+// Parent
+const cardAction = document.querySelector('.card-action');
+
+// Replace (new, old)
+cardAction.replaceChild(newHeading, oldHeading);
+
+// Remove element
+const lis = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// Remove list item
+lis[0].remove();
+
+// Remove Child
+list.removeChild(lis[3]);
+
+// CLASSES & ATTRIBUTES
+
+const firstLi = document.querySelector('li:first-child');
+const link = firstLi.children[0];
+
+let val;
+
+// Classes
+val = link.className;
+console.log(val);
+
+val = link.classList;
+console.log(val);
+
+val = link.classList[0];
+console.log(val);
+
+val = link.classList.add('test');
+val = link;
+console.log(val);
+
+link.classList.remove('test');
+console.log(val);
+
+// Attributes
+val = link.getAttribute('href');
+console.log(val);
+// Check to see link by clicking link 
+val = link.setAttribute('href', 'http://google.com');
+console.log(val);
+
+link.setAttribute('title', 'google');
+console.log(val);
+
+link.removeAttribute('title');
+console.log(val);
+
+// Check to see if there's an attribute
+val = link.hasAttribute('href');
+console.log(val);
+
+
+
+
+
